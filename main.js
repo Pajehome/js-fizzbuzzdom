6e974aed3c41487c9fec5243a637f48b7f11c1a4
Pajehome <abdel20069@gmail.com>
2022-03-28 15:33:51 +0200

    milestone 1/2

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100.
+// MILESTONE 1
+// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// MILESTONE 2
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+// MILESTONE 3
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+let numMax = 100;
+
+
+for(let i = 0;i <= 100;i++) {
+    console.log(i)
+
+    let output = "";
+  if(i % 3 === 0){
+        output += "Flizz"
+        console.log(output)   
+  }else if(i % 5 === 0){
+        output += "Buzz"
+        console.log(output)   
+    }
+  if( i % 3 === 0 && i % 5 === 0 ) {
+        output += "Flizz Buzz"
+        console.log(output)
+    } 
+
+}
+
+
