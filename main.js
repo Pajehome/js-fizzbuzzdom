74d3d033f7a5d73e93b1cada9532d4d15b26f133
Pajehome <abdel20069@gmail.com>
2022-03-29 10:57:14 +0200

    pulizia js

--- main.js
+++ main.js
@@ -8,54 +8,44 @@
 // MILESTONE 3
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
-let numMax = 100;
-
 
 for(let i = 0; i <= 100; i ++) {
-    console.log(i)
+    console.log(i);
 
+    const mioDiv = document.createElement("div");
+    
     let output = "";
 
     if ( i % 3 === 0 && i % 5 === 0 ) {
-        output += "Flizz Buzz"
-        console.log(output)
-
-        const mioDiv = document.createElement("div");
+        output += "Flizz Buzz";
+        console.log(output);
+        
         mioDiv.innerHTML = i + " : " + "E' divisibile sia per 3 che per 5";
-        myDiv.append(mioDiv)
         mioDiv.classList.add("color-3-5");
 
     } else if(i % 3 === 0){
-        output += "Flizz"
-        console.log(output)
+        output += "Flizz";
+        console.log(output);
 
-        const mioDiv = document.createElement("div");
         mioDiv.innerHTML = i + " : " + "E' divisibile solo per 3";
-        myDiv.append(mioDiv)
         mioDiv.classList.add("color-3");
 
     } else if (i % 5 === 0){
-        output += "Buzz"
-        console.log(output) 
-
-        const mioDiv = document.createElement("div");
+        output += "Buzz";
+        console.log(output); 
+        
         mioDiv.innerHTML = i + " : " + "E' divisibile solo per 5";
-        myDiv.append(mioDiv)
         mioDiv.classList.add("color-5");
 
     }else{
-        output += "Non è divisibile ne per 3 ne per 5"
-        console.log(output)
-
-        const mioDiv = document.createElement("div");
-        mioDiv.innerHTML = i + " : " + "Non è divisibile ne per 3 ne per 5";
-        myDiv.append(mioDiv)
-        mioDiv.classList.add("color-none");
+      output += "Non è divisibile ne per 3 ne per 5";
+        console.log(output);
       
-        
-        
+        mioDiv.innerHTML = i + " : " + "Non è divisibile ne per 3 ne per 5";
+        mioDiv.classList.add("color-none");
+ 
     } 
-   
+    myDiv.append(mioDiv);
 }
 
 
